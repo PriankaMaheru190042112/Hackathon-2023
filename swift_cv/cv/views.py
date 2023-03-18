49321060fc43d80afae9d6ebaed7b9db3da0db99
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2023-03-18 22:33:30 +0600

    create cv

--- swift_cv/cv/views.py
+++ swift_cv/cv/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView,TemplateView
 from cv_templates.models import CV_template,CV_type
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
-
+from .models import *
+from  registration.models import User
 # Create your views here.
 def home(request):
     cvs= CV_type.objects.all()
@@ -14,6 +15,47 @@ def home(request):
     
 
 def create_cv(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email= request.POST.get('email')
+        phone= request.POST.get('phone')
+        linked_in= request.POST.get('linked_in')
+        github= request.POST.get('github')
+        image= request.FILES.get('image')
+
+        inst_name = request.POST.getlist('inst_name')
+        result= request.POST.getlist('result')
+        skill= request.POST.getlist('skill_name')
+        ach= request.POST.getlist('ach_name')
+
+       
+        
+        u= CV.objects.create(name=name, email=email, phone_number=phone, linked_in=linked_in , github=github, image=image, )
+        
+        print(inst_name)
+
+        
+        for i in inst_name:
+        
+               inst  = Institution.objects.create(cv_id=u, inst_name=inst_name,result=result)
+               inst.save() 
+
+        for j in skill:
+        
+               skill  = Skill.objects.create(cv_id=u, skill_name=skill)
+               skill.save() 
+        for k in ach:
+        
+               ach  = Achievement.objects.create(cv_id=u, ach_name=ach)
+               ach.save() 
+        
+        
+
+        
+        u.save()
+        return redirect('/create')
+        
+        
     return render(request, 'create_cv.html')
 
 
