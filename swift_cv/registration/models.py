ed0a9288f7949dcf2219d61a80d434ae12f67efb
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2023-03-18 17:25:31 +0600

    user model added

--- swift_cv/registration/models.py
+++ swift_cv/registration/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    isUser = models.BooleanField(default=False, null=False)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30, unique=True)
+    profile_picture = models.ImageField(max_length=200, null=True)
+    isAdmin = models.BooleanField(default=True, null=False)
+ 
